b4b708d9b976bfe210c06dd1f4f8747e837303d7
aashrith <pogulaaashrith67@gmail.com>
2026-07-18 11:46:35 +0530

    commitall

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,36 @@
 // alert("hello  from js")
 console.log("hello from js")
+
+data = {
+    "name" : "john",
+    "gender" : "Male"
+}
+
+data.name
+
+
+var users =[
+    {
+        "name" : "john Doe",
+        "gender" : "Male",
+        "img" : "john.png"
+    },
+    {
+         "name" : "jane Doe",
+        "gender" : "female",
+        "img" : "jane.png"
+    }
+]
+
+
+var id = 0;
+function toggleUser(){
+    id = (id + 1) % users.length;
+    var userimage=document.getElementById("userimage");
+    userimage.src =users[id].img;
+    var username=document.getElementById("user-name");
+    username.textContent = users[id].name;
+    var usergender=document.getElementById("user-gender");
+    usegender.innerHTML = users[id].gender;
+
+}
